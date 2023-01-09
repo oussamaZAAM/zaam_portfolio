fafde208b956837352a7f29b78fa62c7b5693991
oussamaZAAM <zaam.oussama@gmail.com>
2023-01-09 19:39:47 +0100

    add Royal Books

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import bepper from "../../Assets/Projects/bepper.png";
 import qhse from "../../Assets/Projects/qhse.png";
 import rooms from "../../Assets/Projects/rooms.png";
 import tunify from "../../Assets/Projects/tunify.png";
+import royalBooks from "../../Assets/Projects/royalBooks.png";
 
 function Projects() {
   return (
@@ -26,7 +27,7 @@ function Projects() {
               title="Tunify"
               description="Small Application for Music Streaming using Spotify API."
               ghLink="https://github.com/oussamaZAAM/tunify.git"
-              demoLink="pending.com"
+              demoLink="https://tunify.com"
             />
           </Col>
 
@@ -37,7 +38,7 @@ function Projects() {
               title="Bepper"
               description="Personal Project for a Diets & Fitness Application, it calculates the calories of each one and proposes a suitable Diet based on the calories. Built with MERN Stack."
               ghLink="https://github.com/oussamaZAAM/bepper.git"
-              demoLink="bepper.com"
+              demoLink="https://bepper.com"
             />
           </Col>
 
@@ -46,7 +47,7 @@ function Projects() {
               imgPath={rooms}
               isBlog={false}
               title="Rooms"
-              description="Social Network Application with many functionalities: Chat, Posting, creating Rooms."
+              description="Social Network Application with many functionalities: Chat, Posting, creating Rooms. Using MERN Stack and Web Sockets"
               ghLink="https://github.com/oussamaZAAM/Room.git"
               demoLink="https://pfa-rooms.herokuapp.com"              
             />
@@ -57,9 +58,20 @@ function Projects() {
               imgPath={qhse}
               isBlog={false}
               title="QHSE"
-              description="Application for the management of the quality of business services."
+              description="Application for the management of the quality of business services. Built with MERN Stack"
               ghLink="https://github.com/oussamaZAAM/qhse.git"
               demoLink="https://qhse-client.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={royalBooks}
+              isBlog={false}
+              title="Royal Books"
+              description="Application for an online Book Store built with Vanilla JS."
+              ghLink="https://github.com/oussamaZAAM/RoyalBooks.git"
+              demoLink="https://royal-books.com"
             />
           </Col>
         </Row>
